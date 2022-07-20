212a2597b8ceb4970a764d774d7020e40b705e6c
Daniel Kulla <dkulla129@gmail.com>
2022-07-20 14:46:33 -0400

    add an image processing lib

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,29 @@
 const htmlmin = require('html-minifier');
+const Image = require("@11ty/eleventy-img");
+
+async function imageShortcode(src, alt, sizes) {
+  let metadata = await Image(src, {
+    widths: [300, 600],
+    formats: ['jpeg'],
+    outputDir: './public/img/'
+  });
+
+  let imageAttributes = {
+    alt,
+    sizes,
+    loading: "lazy",
+    decoding: "async"
+  }
+  return Image.generateHTML(metadata, imageAttributes, {
+    whitespaceMode: "inline"
+  });
+}
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addWatchTarget('./src/css/tailwind.config.js');
   eleventyConfig.addWatchTarget('./src/css/tailwind.css');
   eleventyConfig.addPassthroughCopy({'./_tmp/style.css': './style.css'});
-
+  eleventyConfig.addNunjucksAsyncShortcode("image", imageShortcode);
 
   eleventyConfig.addTransform('htmlmin', function(content, outputPath) {
     if (
