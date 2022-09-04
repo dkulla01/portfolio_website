3064dcbd2c64e337e956b6dd22ddcbca00347def
dkulla01 <dkulla129@gmail.com>
2022-09-04 11:36:54 -0400

    Homepage (#1)
    let's get the barest-bones eleventy site up

---
+++ .eleventy.js
@@ -0,0 +1,62 @@
+const htmlmin = require('html-minifier');
+const Image = require("@11ty/eleventy-img");
+
+async function imageShortcode(src, attributes) {
+  let metadata = await Image(src, {
+    widths: [300, 600, null],
+    formats: ['jpeg'],
+    outputDir: './public/images/',
+    urlPath: '/images/'
+  });
+
+  let imageAttributes = {
+    ...attributes,
+    loading: "lazy",
+    decoding: "async"
+  };
+
+  return Image.generateHTML(metadata, imageAttributes, {
+    whitespaceMode: "inline"
+  });
+}
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addWatchTarget('./src/css/tailwind.config.js');
+  eleventyConfig.addWatchTarget('./src/css/tailwind.css');
+  eleventyConfig.addPassthroughCopy({'./_tmp/style.css': './style.css'});
+  eleventyConfig.addPassthroughCopy("./src/images")
+  eleventyConfig.addNunjucksAsyncShortcode("image", imageShortcode);
+  let sslKeyPath = process.env.SSL_KEY_PATH;
+  let sslCertPath = process.env.SSL_CERT_PATH;
+  if (sslKeyPath && sslCertPath) {
+    console.log(`using ssl key at ${sslKeyPath} and ssl cert at ${sslCertPath}.`);
+    eleventyConfig.browserSyncConfig = {
+      https: {
+        key: "/Users/dan/.localhost-ssl/dankulla.com.key",
+        cert: "/Users/dan/.localhost-ssl/localhost.crt"
+      }
+    }
+  }
+
+  eleventyConfig.addTransform('htmlmin', function(content, outputPath) {
+    if (
+      process.env.ELEVENTY_PRODUCTION &&
+      outputPath &&
+      outputPath.endsWith('.html')
+    ) {
+      return htmlmin.minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true,
+      });
+    }
+    return content;
+  });
+
+  return {
+    dir: {
+      input: "src",
+      output: "public"
+    }
+  };
+};
---
+++ src/css/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ['public/**/*.html'],
+  safelist: [],
+  theme: {
+    extend: {
+      colors: {
+        change: 'transparent',
+      },
+      maxWidth: {
+        'icon': '64px'
+      }
+    }
+  },
+  plugins: [],
+}
